11439fda4b167a31e28f988fe10b751674b4d5b9
lwmfjc <lwmfjc@gmail.com>
2025-01-01 17:41:02 +0800

    commit_auto massage

--- _sh/init.sh
+++ _sh/init.sh
@@ -40,7 +40,7 @@ if [[ $(uname -a | awk '{print $1}') == 'Linux' ]]; then
       fi
       hugostart_cmd="(cd $docsDir;"' if [[ $(pgrep hugo | wc -l) == 0  ]] ; then '" $hugoParams "' hugo server --minify --environment vmMin --bind 0.0.0.0  ; else echo "hugo 已经在运行" ; fi)' 
    #手机或平板上的termux
-   elif [[ $(uname -m) == "aarch64" ]]; then
+   elif [[ $(uname -m) == "aarch64" || $(uname -m) == "armv7l" ]]; then
       docsDir="/storage/emulated/0/000Ly/git/docs";
       myip=$( ifconfig | grep -P "192(\.\d+){3}" | awk '{print $2}')
       #clear #这里清除了ifconfig的没权限提示
