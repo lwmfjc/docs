ba30c073c35c73b055906be9ec79481453a44d41
lwmfjc <lwmfjc@gmail.com>
2025-01-02 23:46:09 +0800

    中国通史

--- _sh/2BookMd.sh
+++ _sh/2BookMd.sh
@@ -1,8 +1,8 @@
 mv images img
-sed -Ei "s/^##(.*\s)/\1/g" index.txt 
+#sed -Ei "s/^##(.*\s)/\1/g" index.txt 
 # sed不支持PCRE
 # \\\\\[用来取 \[ 
-sed -Ei "s/\\\\\[([0-9]+)\\\\\]/[\1]/g"  index.txt 
+#sed -Ei "s/\\\\\[([0-9]+)\\\\\]/[\1]/g"  index.txt 
 
 #替换图片地址
-sed -Ei "s/\!\[\]\(images/\!\[\]\(img/g"  index.txt 
+sed -Ei "s/(\!\[.*\])\(images/\\1(img/g"  index.txt 
