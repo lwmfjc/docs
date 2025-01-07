40a64522fbf41f60ecc22f769a236e1935202205
lwmfjc <lwmfjc@gmail.com>
2025-01-08 00:36:41 +0800

    css和处理脚本

--- _sh/2BookMd-changeImg.sh
+++ _sh/2BookMd-changeImg.sh
@@ -100,16 +100,17 @@ handleDir(){
 			perl -i -pe 's/\\\[(.*?)\\\]/\[$1\]/gp' "$dirBook/index.txt"
 			perl -i -pe 's/\\\((.*?)\\\)/\($1\)/gp' "$dirBook/index.txt"
 			#标题降1级(#\s+)\*{2}(.*?)\*{4}
-			perl -i -pe 's/^##(.*?\s)/$1/gp' "$dirBook/index.txt"
+			#perl -i -pe 's/^##(.*?\s)/$1/gp' "$dirBook/index.txt"
 
 			#去除#号后面4个星号(#\s+)(.*?)\s*\n\*{4}\s*\n\*{2}(.*)
-			perl -i -0 -pe 's/(#\s+)\*{2}(.*?)\*{4}.*\n\*{4}.*\n\*{2}(.*)\n.*\n/$1$2 $3/gp' "$dirBook/index.txt" 
+			#perl -i -0 -pe 's/(#\s+)\*{2}(.*?)\*{4}.*\n\*{4}.*\n\*{2}(.*)\n.*\n/$1$2 $3/gp' "$dirBook/index.txt" 
+			
+
 			
 			#去除#后面连续的4个星号
 			#perl -i -pe 's/^(#.*?\s)\*{4}/$1/gp' "$dirBook/index.txt" 
 			#去除#号后面4个星号(#\s+)\*{2}(.*?)\*{4}
 			#perl -i -pe 's/(#\s+)\*{2}(.*?)\*{4}/$1$2/gp' "$dirBook/index.txt"
-			
 			#处理img图片
 			# sed -Ei "s/(\!\[\.*?\])\(images/\1\(img/g"  "$dirBook/index.txt"
 			#替换\(\) \[\]之类的默认转义（不需要）
